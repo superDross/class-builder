914e07fd4166250e0bdbea1b90da198fcad25781
David Ross <david.ross@radiuspaymentsolutions.com>
2019-04-21 09:41:03 +0100

    Added php and ruby support

--- lib/class_builder.py
+++ lib/class_builder.py
@@ -22,12 +22,29 @@ class ClassBuilder:
             param_str = f', {param_str}'
         return param_str
 
+    def _ruby_class(self):
+        class_list = [
+            f'class {self.class_name}',
+            f'{self.tab}def initialize({self._param_str()})'
+        ]
+        param_list = [f'{self.tab * 2}@{x} = x' for x in self.params]
+        end = [f'{self.tab}end', 'end']
+        return class_list + param_list + end
+
+    def _php_class(self):
+        class_list = [
+            f'class {self.class_name} {{',
+        ]
+        param_list = [f'{self.tab}public ${x} = {x};' for x in self.params]
+        end = ['}']
+        return class_list + param_list + end
+
     def _python_class(self):
         class_list = [
             f'class {self.class_name}:',
             f'{self.tab}def __init__(self{self._param_str()}):',
         ]
-        param_list = [f'{self.tab * 2}self.{val} = {val}' for val in self.params]
+        param_list = [f'{self.tab * 2}self.{x} = {x}' for x in self.params]
         return class_list + param_list
 
     def _javascript_class(self):
@@ -35,13 +52,16 @@ class ClassBuilder:
             f'class {self.class_name} {{',
             f'{self.tab}constructor({self._param_str()}) {{'
         ]
-        param_list = [f'{self.tab * 2}this.{val} = {val};' for val in self.params]
-        return class_list + param_list + [f'{self.tab}}}', '}']
+        param_list = [f'{self.tab * 2}this.{x} = {x};' for x in self.params]
+        end = [f'{self.tab}}}', '}']
+        return class_list + param_list + end
 
     def build(self):
         method_dict = {
             'py': self._python_class,
-            'js': self._javascript_class
+            'js': self._javascript_class,
+            'rb': self._ruby_class,
+            'php': self._php_class
         }
         if self.ext not in method_dict.keys():
             raise TypeError(f'{self.ext} files are not supported')
